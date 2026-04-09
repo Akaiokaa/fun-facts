f368e034b57b07969070e032270347d8c79b6e56
Akaioka <brendanvillaraza@yahoo.com>
2026-04-09 11:28:17 -0700

    Added PageHeader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import FunFactCard from './FunFactCard';
+import Header from './PageHeader';
 
 function App() {
   return (
     <div>
+      <Header name="Brendan and Allen fun facts! 🎉" />
       <FunFactCard fact="Brendan likes baking."/>
       <FunFactCard fact="Allen likes cars."/>
       <FunFactCard fact="Brendan likes working out."/>
---
+++ src/PageHeader.jsx
@@ -0,0 +1,8 @@
+function Header({name}) {
+    return (
+        <div className="headerName">
+            <h1>{ name }</h1>
+        </div>
+    )
+}
+export default Header;
